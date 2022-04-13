d8da9b111385719df28278d35f7bce738c9d5892
Praveen Kumar <pkkoram98@gmail.com>
2022-04-13 17:14:50 +0530

    wordrow created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import WordRow from "./components/WordRow";
 
 const App: React.FC = () => {
   return (
     <div className="w-96 mx-auto">
-      <h1 className="font-bold text-6xl text-center">Wordle</h1>
+      <header className="border-b border-gray-500 my-2 pb-2">
+        <h1 className="font-bold text-6xl text-center">Wordle</h1>
+      </header>
+
+      <main>
+        <WordRow letters="hel" />
+        <WordRow letters="hell" />
+        <WordRow letters="hello" />
+      </main>
     </div>
   );
 };
---
+++ src/components/WordRow.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const LETTER_LENGTH = 5;
+
+interface WordRowProps {
+  letters: string;
+}
+
+interface CharacterBoxProps {
+  value: string;
+}
+
+const CharacterBox: React.FC<CharacterBoxProps> = ({ value }) => (
+  <span className="border-2 text-center font-extrabold text-4xl p-2 uppercase">
+    {value}
+  </span>
+);
+
+const WordRow: React.FC<WordRowProps> = ({ letters: letterProps }) => {
+  const letterRemaining = LETTER_LENGTH - letterProps.length;
+  const letters = letterProps.split("").concat(Array(letterRemaining).fill(""));
+  return (
+    <div className="grid grid-cols-5 gap-4">
+      {letters.map((char) => (
+        <CharacterBox key={char} value={char} />
+      ))}
+    </div>
+  );
+};
+
+export default WordRow;
