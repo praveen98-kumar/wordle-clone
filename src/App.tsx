75d3bd6efe241f69ce8b93c02e8cb1470067720f
Praveen Kumar <pkkoram98@gmail.com>
2022-04-13 17:00:21 +0530

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import React from "react";
+
+const App: React.FC = () => {
   return (
-    <div>
-      <h1>Wordle</h1>
+    <div className="w-96 mx-auto">
+      <h1 className="font-bold text-6xl text-center">Wordle</h1>
     </div>
   );
-}
+};
 
 export default App;
